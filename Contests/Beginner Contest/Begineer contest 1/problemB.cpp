8c4cc247356c0c816167991cd9e61c9e13ec762f
Sujit Magar <98046388+Sujit-magar@users.noreply.github.com>
2022-11-28 15:56:09 +0530

    Update problemB.cpp

--- Contests/Beginner Contest/Begineer contest 1/problemB.cpp
+++ Contests/Beginner Contest/Begineer contest 1/problemB.cpp
@@ -1,1 +1,19 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s,t; 
+  cin>>s>>t;
+  bool flag=false;
+  for(int i=0;i<s.size();i++){
+    if(s[i]==t[0]){
+      for(int j=0;j<t.size();j++){
+        if(s[i+j]==t[j] && (i+t.size())<=s.size()) {
+          flag=true;
+        }
+        else break;
+      }
+    } }
+  if(flag) cout<<"yes";
+  else cout<<"no";
+  return 0;
+}
